7a881e627a23c39d6473193ed1cad77a7567aab0
Kayo Silva <kayo20git@gmail.com>
2026-06-30 01:28:03 -0300

    feat(quiz): add question display and user answer validation

--- quiz-gpt.py
+++ quiz-gpt.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+
+import json
 from rich.console import Console
 from rich.prompt import Prompt
 
@@ -5,6 +9,15 @@ from rich.prompt import Prompt
 console = Console()
 GREETIING_TITLE = "Bem Vindo ao [bold blue]Quiz PythonGPT![/bold blue]"
 PRESS_ENTER_MESSAGE = "Pressione ENTER para continuar..."
+MOCK_QUEST = {
+  "text": "Você gosta de python?",
+  "choices": [
+    "Sim",
+    "Não",
+  ],
+  "answer": "Sim"
+}
+
 
 def show_greeting(): 
   console.clear()
@@ -18,21 +31,49 @@ def press_enter_to_continue():
   )
 
 
-def create_questions(topic):
-  pass
+def create_question(topic):
+    quest = json.dumps(MOCK_QUEST)
+    return json.loads(quest)
+
+
+def validate_answer(user_points, user_answer, answer):
+  if user_answer == answer:
+      user_points += 1
+      console.print("[bold green]Parabéns! Você acertou![/bold green]")
+      console.print(f"[bold yellow] Você tem {user_points} pontos![/bold yellow]")
+      return user_points
+  else:
+      console.print("[bold red]Que pena! Você errou![/bold red]")
+      console.print(f"A resposta certa é: [bold yellow]{answer}![/bold yellow]")
 
 
 def generate_quiz():
   user_points = 0
-  user_response = ""
+  user_answer = ""
   user_topic = Prompt.ask("Qual tópico você gostaria de estudar hoje?")
 
-  while user_response != "Não":
-    question_created = create_questions(user_topic)
+  while user_answer != "Não":
+    question_created = create_question(user_topic)
+    choices_created = question_created['choices']
+    answer_created = question_created['answer']
 
-    user_response = Prompt.ask(
+    valid_choices = [str(i + 1) for i in range(len(choices_created))]
+
+    console.print(f"[bold green]{question_created['text']}[/bold green]")
+
+    for i, choice in enumerate(choices_created):
+      console.print(f"{i + 1}. {choice}") 
+
+    user_answer_index = int(Prompt.ask(prompt="Qual é a sua resposta?", choices=valid_choices)) -1
+    user_answer = choices_created[user_answer_index]
+
+    console.clear()
+    validate_answer(user_points,user_answer,answer_created)
+
+    user_answer = Prompt.ask(
       prompt="Deseja continuar jogando?",
       choices=["Sim", "Não"],
+      default="Sim"
     )
 
   console.print(user_topic)
